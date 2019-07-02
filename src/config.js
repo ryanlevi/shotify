f95503b9bcf60c81d638ab2d0b76c2ca514946d8
Ryan Levi <ryan.e.levi@gmail.com>
2019-07-01 22:41:31 -0700

    to deploy to heroku

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "47c6325add2a463c828ce524b6880f40";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "https://shotify.herokuapp.com/";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
